867313651aebcc9992f7b9224218fbb928cbe039
Aleksei Timofeev <timap.cpa@gmail.com>
2022-02-14 12:29:51 +0400

    wrote some code in

--- singleton.dart
+++ singleton.dart
@@ -1,3 +1,29 @@
-import 'package:flutter/material.dart';
+class CarSingleton {
+  static CarSingleton? _instance;
+  String name = 'regularCar';
+  int wheel = 4;
+  int steeringWheel = 1;
+  int pedal = 3;
 
+  CarSingleton._();
 
+  static CarSingleton getInstance() {
+    return _instance ??= CarSingleton._();
+  }
+
+  @override
+  String toString {
+    var infoConsole = 'Car name: $name \n';
+    infoConsole = wheel.toString;
+  }
+}
+
+void main(List<String> arguments) {
+  var car = CarSingleton.getInstance();
+  print(car);
+
+  car.name = 'Bolid F1';
+  var newCar = CarSingleton.getInstance();
+  print(identical(car, newCar));
+  print(newCar);
+}
